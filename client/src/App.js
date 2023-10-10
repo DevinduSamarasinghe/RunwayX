3742ae01a6629c50fc25ec81e3beede5909524ea
DevinduSamarasinghe <devindu990@gmail.com>
2023-10-10 15:52:35 +0530

    userinfo page creationg

--- client/src/App.js
+++ client/src/App.js
@@ -30,6 +30,7 @@ import RefundedOrders from "./pages/AdminDashboard/Refunded";
 import ProvisionReport from "./pages/SellerDashboard/ProvisionReport";
 import ItemReport from "./pages/SellerDashboard/ItemReport";
 import OrderReport from "./pages/ClientDashboard/OrderReport";
+import ClientInformation from "./pages/ClientDashboard/ClientInformation";
 
 function App() {
   return (
@@ -52,6 +53,7 @@ function App() {
         <Route path="/failed" element={<Unsuccesful />} />
         <Route path="/mypayments" element={<ClientPayments />} />
         <Route path="/orderReport" element={<OrderReport />} />
+        <Route path="/clientinfo" element={<ClientInformation />} />
         {/* Test method to fetch item information */}
         <Route
           path="/checkout/:6438fa2c518a57cbd5bdc8f4"
